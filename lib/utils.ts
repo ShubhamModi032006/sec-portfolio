e07f7e9546521c26296d9b096411af714692eee1
shubhamiscodding <shubham.modi.cg@gmail.com>
2026-01-24 17:08:22 +0530

    feat: introduce `cn`, `debounce`, and `throttle` utility functions.

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,16 +5,20 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function debounce<T extends (...args: any[]) => void>(func: T, wait: number) {
   let timeout: NodeJS.Timeout;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return function(this: any, ...args: Parameters<T>) {
     clearTimeout(timeout);
     timeout = setTimeout(() => func.apply(this, args), wait);
   };
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function throttle<T extends (...args: any[]) => void>(func: T, limit: number) {
   let inThrottle: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return function(this: any, ...args: Parameters<T>) {
     if (!inThrottle) {
       func.apply(this, args);
